ef8e165805d91876a78356bb376537bda5d6908a
yashinatu <yashinatu2007@gmail.com>
2021-11-11 23:13:53 +0530

    first commit

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Directory from '../../components/directory/directory';
+
+import './homepage.scss';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <h1 className='saya'> SAYA CLOTHING </h1>
+    <Directory />
+  </div>
+);
+
+export default HomePage;
